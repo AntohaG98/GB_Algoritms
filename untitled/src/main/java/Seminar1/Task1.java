2180faaccc1d759182bbe0a85501047af0697bd0
AntohaGaranin <garanin.a@yahoo.com>
2023-05-18 20:29:15 +0300

    Second create

--- untitled/src/main/java/Seminar1/Task1.java
+++ untitled/src/main/java/Seminar1/Task1.java
@@ -1,4 +1,16 @@
 package Seminar1;
-//
+//Необходимо написать алгоритм, считающий сумму всех чисел
+//от 1 до N. Согласно свойствам линейной сложности,
+//количество итераций цикла будет линейно изменяться
+//относительно изменения размера N.
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+    public static void Task1(String[] args){
+
+    }
+
 }
